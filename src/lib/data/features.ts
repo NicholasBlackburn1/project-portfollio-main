28b07a043141aafdf7bed7e54c7fcace0a92941f
nicky blackburn <nickblackburn02@gmail.com>
2024-07-17 18:06:17 -0400

    added fur opdemized stuff

--- src/lib/data/features.ts
+++ src/lib/data/features.ts
@@ -4,33 +4,40 @@ export default [
   {
     name: 'Open Paws & Purrs-onal Privacy',
     description:
-      'I wholeheartedly support open-source projects, believing in the power of collaboration and transparency. By contributing to and promoting open-source initiatives, I aim to foster innovation and community-driven development. Join me in advocating for a tech ecosystem that values openness and integrity!',
+      'I wholeheartedly support open-source projects, believing in the power of collaboration and transparency. By contributing to and promoting open-source initiatives, I aim to foster innovation and community-driven development.',
     image: 'images/features/open-source.jpg',
-    tags: [{ label: 'Open Source' }]
+   
   },
 
   
   {
-    name: 'Themeable',
+    name: 'Pawsitive Integration',
     description:
-      'You can easily theme the entire website by changing just a few colors in the _themes.scss file.',
+      'I creatively integrate solutions into my projects, ensuring each component and feature is designed to enhance functionality and user experience. By implementing innovative approaches and flexible architectures, I strive to deliver dynamic and effective solutions tailored to diverse needs.',
     image: 'images/features/themeable.jpg',
-    tags: [{ label: 'Primary Color' }, { label: 'Secondary Color', color: 'secondary' }]
-  }, 
+    tags: [{ label: 'Innovation' }, { label: 'User Experience' }]
+  },
+
+
   {
-    name: 'Modular Development',
+    name: 'Modular Mischief',
     description:
       'I embrace a modular development approach across all my projects, enabling easy integration of new features and functionalities. Whether in web design, software development, or AI projects, modularity ensures flexibility and scalability.',
     image: 'images/features/extensible.jpg',
-},
+    tags: [
+      { label: 'Flexibility' },
+      { label: 'Scalability' },]
+  },
+
 
   {
-    name: 'Well Optimized',
+    name: 'Fur-Optimized',
     description:
-      'Images are automatically optimized and lazy loaded, to ensure the website loads as fast as possible regardless of connection speed.',
+      'I meticulously optimize all my projects, ensuring swift performance and seamless user experiences. By utilizing advanced techniques like automatic fluffification and lazy loading, I enhance efficiency and accessibility across diverse platforms and applications.',
     image: 'images/features/optimized.jpg',
-    tags: [{ label: 'Powered by Image Transmutation' }]
-  }, 
+    tags: [{ label: 'Efficiency' }, { label: 'Performance' }, { label: 'User Experience' }]
+
+  },
 
   
 ] as Feature[];
